122ab8c443d45ad43f79cdf63776ccd27aad976e
VovaYakymovych <yakym.volodya@gmail.com>
2025-03-26 01:23:04 +0200

    hw2 upd

--- hw2/hw10/hw10.1/hw10.ts
+++ hw2/hw10/hw10.1/hw10.ts
@@ -38,18 +38,19 @@ formButton.innerText = 'Submit'
 formAge.onsubmit = function (ev:SubmitEvent):void {
   ev.preventDefault()
 
-  let age:string = inputAge.value
+  let ageInput:HTMLInputElement = document.getElementById('inputAge') as HTMLInputElement
+  let age: number = parseInt(ageInput.value, 10)
 
-  let existingWarning:HTMLParagraphElement|null = document.getElementById('ageWarning');
+  let existingWarning:HTMLParagraphElement = document.getElementById('ageWarning') as HTMLParagraphElement
   if (existingWarning) {
-    existingWarning.remove();
+    existingWarning.remove()
   }
 
-  let warning = document.createElement('p')
+  let warning:HTMLParagraphElement = document.createElement('p')
   warning.id = 'ageWarning'
   document.body.appendChild(warning)
 
-  if (age === '') {
+  if (isNaN(ageInput.valueAsNumber)) {
     warning.textContent = 'Enter your age'
   } else if (age < 18) {
     warning.textContent = 'Your age is below 18'
@@ -60,224 +61,246 @@ formAge.onsubmit = function (ev:SubmitEvent):void {
 // ==================================task #ymAmN2xJ======================================
 
 
-// let formUser = document.createElement('form')
-// let userName = document.createElement('input')
-// let userSurname = document.createElement('input')
-// let userAge = document.createElement('input')
-// let formUserButton = document.createElement('button')
-//
-// formUser.append(userName, userSurname, userAge, formUserButton)
-// document.body.appendChild(formUser)
-//
-// formUser.classList.add('form-style-preset')
-// userName.id = 'formUserName'
-// userSurname.id = 'formUserSurname'
-// userAge.id = 'formUserAge'
-// userAge.type = 'number'
-// formUserButton.classList.add('formButton')
-//
-// document.querySelectorAll("input").forEach(input => {
-//   input.classList.add("formInput");
-// });
-//
-// userName.placeholder = 'Enter your name'
-// userSurname.placeholder = 'Enter your surname'
-// userAge.placeholder = 'Enter your age'
-// formUserButton.innerText = 'Submit'
-//
-// formUser.onsubmit = function (ev) {
-//   ev.preventDefault()
-//
-//   let user = {
-//     name: document.getElementById('formUserName').value,
-//     surname: document.getElementById('formUserSurname').value,
-//     age: document.getElementById('formUserAge').value
-//   };
-//
-//   let userDataBlock = document.createElement('div');
-//   userDataBlock.id = 'userDataBlock';
-//
-//   userDataBlock.innerHTML = `
-//         <p>Name: ${user.name || 'None'}</p>
-//         <p>Surname: ${user.surname || 'None'}</p>
-//         <p>Age: ${user.age || 'None'}</p>
-//     `;
-//
-//   document.body.appendChild(userDataBlock);
-// }
+let formUser:HTMLFormElement = document.createElement('form')
+let userName:HTMLInputElement = document.createElement('input')
+let userSurname:HTMLInputElement = document.createElement('input')
+let userAge:HTMLInputElement = document.createElement('input')
+let formUserButton:HTMLButtonElement = document.createElement('button')
+
+formUser.append(userName, userSurname, userAge, formUserButton)
+document.body.appendChild(formUser)
+
+formUser.classList.add('form-style-preset')
+userName.id = 'formUserName'
+userSurname.id = 'formUserSurname'
+userAge.id = 'formUserAge'
+userAge.type = 'number'
+formUserButton.classList.add('formButton')
+
+document.querySelectorAll("input").forEach(input => {
+  input.classList.add("formInput");
+});
+
+userName.placeholder = 'Enter your name'
+userSurname.placeholder = 'Enter your surname'
+userAge.placeholder = 'Enter your age'
+formUserButton.innerText = 'Submit'
+
+interface IUser4{
+  name:string
+  surname:string
+  age:number
+}
+
+formUser.onsubmit = function (ev:SubmitEvent):void {
+  ev.preventDefault()
+
+  let nameInput:HTMLInputElement = document.getElementById('formUserName') as HTMLInputElement
+  let surnameInput:HTMLInputElement = document.getElementById('formUserSurname') as HTMLInputElement
+  let ageInput:HTMLInputElement = document.getElementById('formUserAge') as HTMLInputElement
+
+  let user: IUser4 = {
+    name: nameInput.value.trim(),
+    surname: surnameInput.value.trim(),
+    age: parseInt(ageInput.value, 10)
+  };
+
+  let userDataBlock:HTMLDivElement = document.createElement('div');
+  userDataBlock.id = 'userDataBlock';
+
+  userDataBlock.innerHTML = `
+        <p>Name: ${user.name || 'None'}</p>
+        <p>Surname: ${user.surname || 'None'}</p>
+        <p>Age: ${user.age || 'None'}</p>
+    `;
+
+  document.body.appendChild(userDataBlock);
+}
 
 
 // ==================================task #2VaLt4vDczH======================================
 
 
-// let counterDiv = document.createElement('div')
-// counterDiv.id = 'counter'
-// document.body.appendChild(counterDiv)
-//
-// let counter = localStorage.getItem('pageCounter') ? parseInt(localStorage.getItem('pageCounter')) : 0
-//
-// counter++
-//
-// localStorage.setItem('pageCounter', counter)
-// document.getElementById('counter').innerText = counter
+let counterDiv:HTMLDivElement = document.createElement('div');
+counterDiv.id = 'counter';
+document.body.appendChild(counterDiv);
+
+let counter: number = parseInt(localStorage.getItem('pageCounter') || '0', 10);
+
+counter++;
+localStorage.setItem('pageCounter', counter.toString());
+
+counterDiv.innerText = counter.toString();
+
 
 
 // ==================================task #LhSfdhM3======================================
 
-// let sessionList = JSON.parse(localStorage.getItem('sessionList'))||[]
-// let visit = {date:new Date().toLocaleString()}
-//
-// sessionList.push(visit)
-// localStorage.setItem('sessionList', JSON.stringify(sessionList))
+interface IVisit {
+  date: string;
+}
+
+let sessionList:IVisit[] = JSON.parse(localStorage.getItem('sessionList')||'[]') as IVisit[]
+let visit:IVisit = {date:new Date().toLocaleString()}
+
+sessionList.push(visit)
+localStorage.setItem('sessionList', JSON.stringify(sessionList))
 
 
 // ==================================task #Jg0gPO00======================================
 
 
-// let kgInput = document.createElement('input')
-// let resultOutput = document.createElement('div')
-//
-// kgInput.id = 'kgInput'
-// kgInput.classList.add('formInput')
-// kgInput.placeholder = 'Enter kg amount'
-//
-// resultOutput.id = 'result'
-//
-// kgInput.addEventListener('input',function (){
-//   let kg = parseFloat(kgInput.value);
-//
-//   let pounds = kg * 2.20462;
-//   resultOutput.textContent = `Weight in pounds: ${pounds.toFixed(2)}`;
-// })
-//
-// document.body.append(kgInput,resultOutput)
+let kgInput:HTMLInputElement = document.createElement('input')
+let resultOutput:HTMLDivElement = document.createElement('div')
+
+kgInput.id = 'kgInput'
+kgInput.classList.add('formInput')
+kgInput.placeholder = 'Enter kg amount'
+
+resultOutput.id = 'result'
+
+kgInput.addEventListener('input',function ():void{
+  let kg:number = parseFloat(kgInput.value);
+
+  let pounds:number = kg * 2.20462;
+  resultOutput.textContent = `Weight in pounds: ${pounds.toFixed(2)}`;
+})
+
+document.body.append(kgInput,resultOutput)
 
 
 // ==================================task #RbQGnH5DuC======================================
 
+interface IUser5 {
+  name:string
+  age:number
+}
 
-// let addToLocalStorage = function (arrayName, objToAdd) {
-//
-//   let arr = JSON.parse(localStorage.getItem(arrayName)) || [];
-//   arr.push(objToAdd)
-//
-//   localStorage.setItem(arrayName, JSON.stringify(arr))
-// }
-//
-// addToLocalStorage('list1', {name: 'vasya', age: 31})
+function addToLocalStorage (arrayName:string, objToAdd:IUser5):void {
 
+  let arr:IUser5[] = JSON.parse(localStorage.getItem(arrayName) || '[]') as IUser5[]
+  arr.push(objToAdd)
 
-// ==================================task #kUSgFqWY======================================
+  localStorage.setItem(arrayName, JSON.stringify(arr))
+}
 
-// let tableForm = document.createElement('form')
-// let rowInput = document.createElement('input')
-// let columnInput = document.createElement('input')
-// let infoInput = document.createElement('input')
-// let tableFormButton = document.createElement('button')
-// let tableContainer = document.createElement('div')
-//
-// tableForm.append(rowInput, columnInput, infoInput, tableFormButton)
-// document.body.append(tableForm, tableContainer)
-//
-// tableForm.classList.add('form-style-preset')
-// tableFormButton.classList.add('formButton')
-//
-// document.querySelectorAll("input").forEach(input => {
-//   input.classList.add("formInput")
-// })
-//
-// tableForm.id = 'tableForm'
-// rowInput.id = 'rowInput'
-// columnInput.id = 'columnInput'
-// infoInput.id = 'infoInput'
-//
-// rowInput.type ='number'
-// columnInput.type ='number'
-//
-// rowInput.placeholder = 'Enter amount of rows'
-// columnInput.placeholder = 'Enter amount of columns'
-// infoInput.placeholder = 'Enter info to display in table'
-// tableFormButton.innerText = 'Submit'
-//
-// tableForm.onsubmit = function (ev) {
-//   ev.preventDefault()
-//
-//   let rows = parseInt(rowInput.value)
-//   let columns = parseInt(columnInput.value)
-//   let info = infoInput.value
-//
-//   tableContainer.innerHTML = ""
-//
-//   tableContainer.style.display = "grid"
-//   tableContainer.style.gridTemplateColumns = `repeat(${columns}, 100px)`
-//
-//   for (let i = 0; i < rows * columns; i++) {
-//     let cell = document.createElement('div')
-//     cell.classList.add('cell')
-//     cell.innerText = info
-//     tableContainer.appendChild(cell)
-//   }
-// }
+addToLocalStorage('list1', {name: 'vasya', age: 31})
 
 
 // ==================================task #kUSgFqWY======================================
 
-// let container = document.createElement('div')
-// let buttonsWrap = document.createElement('div')
-// let prevButton = document.createElement('button')
-// let nextButton = document.createElement('button')
-//
-// container.id = 'container'
-// buttonsWrap.classList.add('buttons')
-// prevButton.id = 'prev'
-// nextButton.id = 'next'
-//
-// prevButton.innerText = 'Prev'
-// nextButton.innerText = 'Next'
-//
-// buttonsWrap.append(prevButton, nextButton)
-// document.body.append(container, buttonsWrap)
-//
-//
-// let items = []
-// for (let i = 1; i <= 100; i++) {
-//   items.push({ id: i, name: "Item " + i })
-// }
-//
-// let currentPage = 0
-// let itemsPerPage = 10
-//
-// function renderItems() {
-//   container.innerHTML = ""
-//
-//   let start = currentPage * itemsPerPage
-//   let end = start + itemsPerPage
-//   let pageItems = items.slice(start, end)
-//
-//   pageItems.forEach(item => {
-//     let div = document.createElement('div')
-//     div.classList.add('item')
-//     div.innerText = `${item.id}: ${item.name}`
-//     container.appendChild(div)
-//   })
-//
-//   prevButton.disabled = currentPage === 0
-//   nextButton.disabled = end >= items.length
-// }
-//
-// prevButton.onclick = function () {
-//   if (currentPage > 0) {
-//     currentPage--
-//     renderItems()
-//   }
-// }
-//
-// nextButton.onclick = function () {
-//   if ((currentPage + 1) * itemsPerPage < items.length) {
-//     currentPage++
-//     renderItems()
-//   }
-// }
-//
-//
-// renderItems()
+let tableForm:HTMLFormElement = document.createElement('form')
+let rowInput:HTMLInputElement = document.createElement('input')
+let columnInput:HTMLInputElement = document.createElement('input')
+let infoInput:HTMLInputElement = document.createElement('input')
+let tableFormButton:HTMLButtonElement = document.createElement('button')
+let tableContainer:HTMLDivElement = document.createElement('div')
+
+tableForm.append(rowInput, columnInput, infoInput, tableFormButton)
+document.body.append(tableForm, tableContainer)
+
+tableForm.classList.add('form-style-preset')
+tableFormButton.classList.add('formButton')
+
+document.querySelectorAll("input").forEach(input => {
+  input.classList.add("formInput")
+})
+
+tableForm.id = 'tableForm'
+rowInput.id = 'rowInput'
+columnInput.id = 'columnInput'
+infoInput.id = 'infoInput'
+
+rowInput.type ='number'
+columnInput.type ='number'
+
+rowInput.placeholder = 'Enter amount of rows'
+columnInput.placeholder = 'Enter amount of columns'
+infoInput.placeholder = 'Enter info to display in table'
+tableFormButton.innerText = 'Submit'
+
+tableForm.onsubmit = function (ev:SubmitEvent):void {
+  ev.preventDefault()
+
+  let rows:number = parseInt(rowInput.value)
+  let columns:number = parseInt(columnInput.value)
+  let info:string = infoInput.value
+
+  tableContainer.innerHTML = ""
+
+  tableContainer.style.display = "grid"
+  tableContainer.style.gridTemplateColumns = `repeat(${columns}, 100px)`
+
+  for (let i:number = 0; i < rows * columns; i++) {
+    let cell:HTMLDivElement = document.createElement('div')
+    cell.classList.add('cell')
+    cell.innerText = info
+    tableContainer.appendChild(cell)
+  }
+}
+
+
+// ==================================task #kUSgFqWY======================================
+
+let container:HTMLDivElement = document.createElement('div')
+let buttonsWrap:HTMLDivElement = document.createElement('div')
+let prevButton:HTMLButtonElement = document.createElement('button')
+let nextButton:HTMLButtonElement = document.createElement('button')
+
+container.id = 'container'
+buttonsWrap.classList.add('buttons')
+prevButton.id = 'prev'
+nextButton.id = 'next'
+
+prevButton.innerText = 'Prev'
+nextButton.innerText = 'Next'
+
+buttonsWrap.append(prevButton, nextButton)
+document.body.append(container, buttonsWrap)
+
+type itemsType ={
+  id:number
+  name:string
+}
+
+let items:itemsType[] = []
+for (let i:number = 1; i <= 100; i++) {
+  items.push({ id: i, name: "Item " + i })
+}
+
+let currentPage:number = 0
+let itemsPerPage:number = 10
+
+function renderItems():void {
+  container.innerHTML = ""
+
+  let start:number = currentPage * itemsPerPage
+  let end:number = start + itemsPerPage
+  let pageItems:itemsType[] = items.slice(start, end)
+
+  pageItems.forEach(item => {
+    let div:HTMLDivElement = document.createElement('div')
+    div.classList.add('item')
+    div.innerText = `${item.id}: ${item.name}`
+    container.appendChild(div)
+  })
+
+  prevButton.disabled = currentPage === 0
+  nextButton.disabled = end >= items.length
+}
+
+prevButton.onclick = function ():void {
+  if (currentPage > 0) {
+    currentPage--
+    renderItems()
+  }
+}
+
+nextButton.onclick = function ():void {
+  if ((currentPage + 1) * itemsPerPage < items.length) {
+    currentPage++
+    renderItems()
+  }
+}
+
+renderItems()
--- hw2/hw10/sessionsListPage/sessionsListPage.ts
+++ hw2/hw10/sessionsListPage/sessionsListPage.ts
@@ -1,16 +1,16 @@
 
 
 
-// let sessionsList: = JSON.parse(localStorage.getItem('sessionList')) || [];
-// let listContainer:HTMLUListElement|null = document.getElementById('sessionsList');
-//
-//
-// if (sessionsList.length === 0) {
-//     listContainer.innerHTML = "<p>No visits</p>";
-// } else {
-//     sessionsList.forEach(visit => {
-//         let listItem = document.createElement('li');
-//         listItem.textContent = `Page visited on: ${visit.date}`;
-//         listContainer.appendChild(listItem);
-//     });
-// }
+let sessionsList:IVisit[] = JSON.parse(localStorage.getItem('sessionList') || '[]') as IVisit[] ;
+let listContainer:HTMLUListElement = document.getElementById('sessionsList') as HTMLUListElement;
+
+
+if (sessionsList.length === 0) {
+    listContainer.innerHTML = "<p>No visits</p>";
+} else {
+    sessionsList.forEach(visit => {
+        let listItem:HTMLLIElement = document.createElement('li');
+        listItem.textContent = `Page visited on: ${visit.date}`;
+        listContainer.appendChild(listItem);
+    });
+}
